074859c2b60ee5a7020b0552d59a2dcb864f71fb
Juanito Banca <jose_alvarado89@hotmail.com>
2021-04-19 19:44:02 -0700

    Additional PF's
    SO, UBB, FB, GO, LD

--- data/park_factors/procedimientos/pf_park_factors.sql
+++ data/park_factors/procedimientos/pf_park_factors.sql
@@ -40,7 +40,12 @@ INSERT INTO pf_park_factors(
     singlesParkFactor,
     doublesParkFactor,
     triplesParkFactor,
-    homeRunsParkFactor
+    homeRunsParkFactor,
+    strikeOutsParkFactor,
+    unintentionalWalksParkFactor,
+    flyBallsParkFactor,
+    groundBallsParkFactor,
+    lineDrivesParkFactor
   )
   WITH home_scored AS (
     SELECT
@@ -56,6 +61,11 @@ INSERT INTO pf_park_factors(
       doubles AS doublesScoredHome,
       triples AS triplesScoredHome,
       homeRuns AS homeRunsScoredHome,
+      strikeOuts AS strikeOutsScoredHome,
+      unintentionalWalks AS unintentionalWalksScoredHome,
+      flyBalls AS flyBallsScoredHome,
+      groundBalls AS groundBallsScoredHome,
+      lineDrives AS lineDrivesScoredHome,
       games AS homeGames
     FROM agg_batting_stats
     WHERE
@@ -75,6 +85,11 @@ INSERT INTO pf_park_factors(
       doubles AS doublesScoredAway,
       triples AS triplesScoredAway,
       homeRuns AS homeRunsScoredAway,
+      strikeOuts AS strikeOutsScoredAway,
+      unintentionalWalks AS unintentionalWalksScoredAway,
+      flyBalls AS flyBallsScoredAway,
+      groundBalls AS groundBallsScoredAway,
+      lineDrives AS lineDrivesScoredAway,
       games AS awayGames
     FROM agg_batting_stats
     WHERE
@@ -93,7 +108,12 @@ INSERT INTO pf_park_factors(
       singles AS singlesAllowedHome,
       doubles AS doublesAllowedHome,
       triples AS triplesAllowedHome,
-      homeRuns AS homeRunsAllowedHome
+      homeRuns AS homeRunsAllowedHome,
+      strikeOuts AS strikeOutsAllowedHome,
+      unintentionalWalks AS unintentionalWalksAllowedHome,
+      flyBalls AS flyBallsAllowedHome,
+      groundBalls AS groundBallsAllowedHome,
+      lineDrives AS lineDrivesAllowedHome
     FROM agg_pitching_stats
     WHERE
       groupingDescription = 'MAJORLEAGUEID_SEASONID_GAMETYPE2_VENUEID_TEAMID_TEAMTYPE'
@@ -111,7 +131,12 @@ INSERT INTO pf_park_factors(
       singles AS singlesAllowedAway,
       doubles AS doublesAllowedAway,
       triples AS triplesAllowedAway,
-      homeRuns AS homeRunsAllowedAway
+      homeRuns AS homeRunsAllowedAway,
+      strikeOuts AS strikeOutsAllowedAway,
+      unintentionalWalks AS unintentionalWalksAllowedAway,
+      flyBalls AS flyBallsAllowedAway,
+      groundBalls AS groundBallsAllowedAway,
+      lineDrives AS lineDrivesAllowedAway
     FROM agg_pitching_stats
     WHERE
       groupingDescription = 'MAJORLEAGUEID_SEASONID_GAMETYPE2_TEAMID_TEAMTYPE'
@@ -147,6 +172,26 @@ SELECT
   homeRunsAllowedHome,
   homeRunsScoredAway,
   homeRunsAllowedAway,
+  strikeOutsScoredHome,
+  strikeOutsAllowedHome,
+  strikeOutsScoredAway,
+  strikeOutsAllowedAway,
+  unintentionalWalksScoredHome,
+  unintentionalWalksAllowedHome,
+  unintentionalWalksScoredAway,
+  unintentionalWalksAllowedAway,
+  flyBallsScoredHome,
+  flyBallsAllowedHome,
+  flyBallsScoredAway,
+  flyBallsAllowedAway,
+  groundBallsScoredHome,
+  groundBallsAllowedHome,
+  groundBallsScoredAway,
+  groundBallsAllowedAway,
+  lineDrivesScoredHome,
+  lineDrivesAllowedHome,
+  lineDrivesScoredAway,
+  lineDrivesAllowedAway,
   IF( runsScoredAway + runsAllowedAway > 0,
     ((runsScoredHome + runsAllowedHome) / homeGames) /
     ((runsScoredAway + runsAllowedAway) / awayGames),
@@ -171,7 +216,32 @@ SELECT
     ((homeRunsScoredHome + homeRunsAllowedHome) / homeGames) /
     ((homeRunsScoredAway + homeRunsAllowedAway) / awayGames),
     NULL
-   ) AS homeRunsParkFactor
+   ) AS homeRunsParkFactor,
+  IF( strikeOutsScoredAway + strikeOutsAllowedAway > 0,
+    ((strikeOutsScoredHome + strikeOutsAllowedHome) / homeGames) /
+    ((strikeOutsScoredAway + strikeOutsAllowedAway) / awayGames),
+    NULL
+   ) AS strikeOutsParkFactor,
+  IF( unintentionalWalksScoredAway + unintentionalWalksAllowedAway > 0,
+    ((unintentionalWalksScoredHome + unintentionalWalksAllowedHome) / homeGames) /
+    ((unintentionalWalksScoredAway + unintentionalWalksAllowedAway) / awayGames),
+    NULL
+   ) AS unintentionalWalksParkFactor,
+  IF( flyBallsScoredAway + flyBallsAllowedAway > 0,
+    ((flyBallsScoredHome + flyBallsAllowedHome) / homeGames) /
+    ((flyBallsScoredAway + flyBallsAllowedAway) / awayGames),
+    NULL
+   ) AS flyBallsParkFactor,
+  IF( groundBallsScoredAway + groundBallsAllowedAway > 0,
+    ((groundBallsScoredHome + groundBallsAllowedHome) / homeGames) /
+    ((groundBallsScoredAway + groundBallsAllowedAway) / awayGames),
+    NULL
+   ) AS groundBallsParkFactor,
+  IF( lineDrivesScoredAway + lineDrivesAllowedAway > 0,
+    ((lineDrivesScoredHome + lineDrivesAllowedHome) / homeGames) /
+    ((lineDrivesScoredAway + lineDrivesAllowedAway) / awayGames),
+    NULL
+   ) AS lineDrivesParkFactor
 FROM home_scored hs
 INNER JOIN away_scored aws
   ON hs.seasonId = aws.seasonId
